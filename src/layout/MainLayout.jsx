73430252a896c0b75b162830dc443cd029f977ee
Arrafi2644 <raffian2644@gmail.com>
2025-02-02 23:55:14 +0600

    Change text color

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom';
 
 const MainLayout = () => {
     return (
-        <div className='bg-background font-open-sans'>
+        <div className='bg-background font-open-sans text-gray-800'>
             <Navbar></Navbar>
             <div className='max-w-7xl w-11/12 mx-auto'>
                 <Outlet></Outlet>
